ae13f41971dee7ab718103aafe1d9d93eb1c468d
Ram-Amoncar <ramamonkar444@gmail.com>
2024-01-14 14:29:27 +0530

    Solved fruit-counter

---
+++ src/fruit-counter/my-solutions/forof.fruit-counter.spec.ts
@@ -0,0 +1,18 @@
+import { describe, it, expect } from 'vitest'
+import { getFruitCount } from './forof.fruit-counter'
+
+describe('fruitCounter', () => {
+  it('should return a count of all repeated elements', () => {
+    const given = ['🍋', '🍉', '🍒', '🍋', '🍋', '🍎', '🍎', '🍐']
+
+    const actual = getFruitCount(given)
+
+    expect(actual).toMatchObject({
+      '🍒': 1,
+      '🍋': 3,
+      '🍉': 1,
+      '🍎': 2,
+      '🍐': 1
+    })
+  })
+})
---
+++ src/fruit-counter/my-solutions/forof.fruit-counter.ts
@@ -0,0 +1,11 @@
+function getFruitCount(fruits: string[]): object {
+  type fruitCounter = Record<string, number>
+  let counter: fruitCounter = {}
+  for (const fruit of fruits) {
+    if (counter.hasOwnProperty(fruit)) counter[fruit]++
+    else counter[fruit] = 1
+  }
+  return counter
+}
+
+export { getFruitCount }
